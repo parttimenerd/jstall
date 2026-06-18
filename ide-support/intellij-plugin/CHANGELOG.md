443c71704ded1e11b02ba066c9ff7fa71ab08903
Johannes Bechberger <johannes.bechberger@sap.com>
2026-06-18 11:45:27 +0200

    feat: consolidate IntelliJ and VS Code plugins into ide-support/
    Moves jstall-intellij-plugin and jstall-vscode-extension into
    ide-support/intellij-plugin and ide-support/vscode-extension respectively.
    
    Adds adapted CI workflows (ide-intellij-build, ide-intellij-release,
    ide-intellij-ui-tests, ide-vscode-build, ide-vscode-release) with
    path filters and working-directory scoped to their subdirectory.
    
    Release tags use intellij-<version> and vscode-v<version> prefixes
    to avoid collisions with the main jstall release tags.
    
    The two standalone repos will be archived with a pointer to this location.

---
+++ ide-support/intellij-plugin/CHANGELOG.md
@@ -0,0 +1,10 @@
+<!-- Keep a Changelog guide -> https://keepachangelog.com -->
+
+# JStall IntelliJ Plugin Changelog
+
+## [Unreleased]
+
+## [0.1.0]
+
+### Added
+- Initial implementation
---
+++ ide-support/intellij-plugin/src/main/kotlin/me/bechberger/jstall/JStallIcons.kt
@@ -0,0 +1,8 @@
+package me.bechberger.jstall
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object JStallIcons {
+    @JvmField val RecordingFile: Icon = IconLoader.getIcon("/icons/jstallRecordingFile.svg", JStallIcons::class.java)
+}
